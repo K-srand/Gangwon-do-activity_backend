0b5889e33d6ced87b5b7e45d5d5f704f61aeff0b
pmhlj <minhobag794@gmail.com>
2024-07-01 16:12:26 +0900

    boardCRU

---
+++ src/main/java/com/multicampus/ganwonActivity/controller/BoardController.java
@@ -0,0 +1,64 @@
+package com.multicampus.ganwonActivity.controller;
+
+
+import com.multicampus.ganwonActivity.dto.request.board.PatchBoardRequestDto;
+import com.multicampus.ganwonActivity.dto.request.board.PostBoardRequestDto;
+import com.multicampus.ganwonActivity.dto.response.board.PatchBoardResponseDto;
+import com.multicampus.ganwonActivity.dto.response.board.PostBoardResponseDto;
+import com.multicampus.ganwonActivity.entity.BoardEntity;
+import com.multicampus.ganwonActivity.service.BoardService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/board")
+public class BoardController {
+
+
+    private final BoardService boardService;
+
+    @PostMapping("")
+    public ResponseEntity<? super PostBoardResponseDto> postBoard(
+            @RequestBody @Valid PostBoardRequestDto postBoardRequestDto,
+            @AuthenticationPrincipal String id
+            ){
+
+            ResponseEntity<? super PostBoardResponseDto> postBoardResponseDto = boardService.postBoard(postBoardRequestDto, id);
+            return postBoardResponseDto;
+
+    }
+
+    @PatchMapping("/{boardNo}")
+    public ResponseEntity<? super PatchBoardResponseDto> patchBoard(
+            @PathVariable("boardNo") Long boardNo,
+            @RequestBody @Valid PatchBoardRequestDto patchBoardRequestDto,
+            @AuthenticationPrincipal String id){
+
+        ResponseEntity<? super PatchBoardResponseDto> patchBoardResponseDto = boardService.patchBoard(boardNo, patchBoardRequestDto, id);
+        return patchBoardResponseDto;
+
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<List<BoardEntity>> listBoard(){
+        List<BoardEntity> boardEntityList = boardService.listBoard();
+        return ResponseEntity.ok(boardEntityList);
+
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteBoard(@AuthenticationPrincipal String id)
+    {
+        boardService.deleteBoard(id);
+        return "";
+    }
+
+
+
+}
---
+++ src/main/java/com/multicampus/ganwonActivity/service/BoardService.java
@@ -0,0 +1,20 @@
+package com.multicampus.ganwonActivity.service;
+
+import com.multicampus.ganwonActivity.dto.request.board.PatchBoardRequestDto;
+import com.multicampus.ganwonActivity.dto.request.board.PostBoardRequestDto;
+import com.multicampus.ganwonActivity.dto.response.board.PatchBoardResponseDto;
+import com.multicampus.ganwonActivity.dto.response.board.PostBoardResponseDto;
+import com.multicampus.ganwonActivity.entity.BoardEntity;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public interface BoardService {
+    ResponseEntity<? super PostBoardResponseDto> postBoard(PostBoardRequestDto dto, String id);
+
+    ResponseEntity<? super PatchBoardResponseDto> patchBoard(Long boardNo, PatchBoardRequestDto dto, String id);
+
+    List<BoardEntity> listBoard();
+
+    void deleteBoard(String id);
+}
