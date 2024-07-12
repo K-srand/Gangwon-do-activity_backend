b0924ec074ad8d233de5f8b6c1005db16d6fd9f6
K-srand <ksuji9278@gmail.com>
2024-07-12 17:28:06 +0900

    clean

--- gangwonActivity/src/main/java/com/multicampus/gangwonActivity/repository/BoardLikesMappingTableRepository.java
+++
@@ -1,7 +0,0 @@
-package com.multicampus.gangwonActivity.repository;
-
-import com.multicampus.gangwonActivity.entity.BoardLikesMappingTableEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardLikesMappingTableRepository extends JpaRepository<BoardLikesMappingTableEntity, Long> {
-}
--- gangwonActivity/src/main/java/com/multicampus/gangwonActivity/repository/Tour4_0Repository.java
+++
@@ -1,7 +0,0 @@
-package com.multicampus.gangwonActivity.repository;
-
-import com.multicampus.gangwonActivity.entity.Tour4_0Entity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Tour4_0Repository extends JpaRepository<Tour4_0Entity, Long>  {
-}
