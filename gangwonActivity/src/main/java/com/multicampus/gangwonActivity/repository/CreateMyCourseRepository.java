d931eba2647fd2072fa76f34cc6f46244a4aa0af
K-srand <ksuji9278@gmail.com>
2024-07-17 21:30:13 +0900

    clean

--- gangwonActivity/src/main/java/com/multicampus/gangwonActivity/repository/CreateMyCourseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.multicampus.gangwonActivity.repository;
-
-import com.multicampus.gangwonActivity.entity.MyCoursePlace;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CreateMyCourseRepository extends JpaRepository<MyCoursePlace, Long> {
-
-}
