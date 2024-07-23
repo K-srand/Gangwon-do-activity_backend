902f69f6f2d76da9538d6ee561ba91fefe6c1d78
K-srand <ksuji9278@gmail.com>
2024-07-23 10:08:35 +0900

    테스트3

--- gangwonActivity/src/main/java/com/multicampus/gangwonActivity/repository/BoardLikesMappingTableRepository.java
+++
@@ -1,7 +0,0 @@
-package com.multicampus.gangwonActivity.repository;
-
-import com.multicampus.gangwonActivity.entity.BoardLikesUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardLikesMappingTableRepository extends JpaRepository<BoardLikesUser, Long> {
-}
