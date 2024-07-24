2caa4461c89e864f50ac90c9c016dca8265246a5
K-srand <ksuji9278@gmail.com>
2024-07-25 06:01:28 +0900

    빌드 에러 수정중

---
+++ src/main/java/com/multicampus/gangwonActivity/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.multicampus.gangwonActivity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://223.130.138.174:3030")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
