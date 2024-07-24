74e9bed3cc20362b3af9907e3d640a9b26e919d2
K-srand <ksuji9278@gmail.com>
2024-07-25 03:11:45 +0900

    빌드 에러 수정중

--- src/main/java/com/multicampus/gangwonActivity/controller/AuthController.java
+++ src/main/java/com/multicampus/gangwonActivity/controller/AuthController.java
@@ -28,7 +28,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 
-@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
+@CrossOrigin(origins = "http://223.130.138.174:3030", allowCredentials = "true")
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
