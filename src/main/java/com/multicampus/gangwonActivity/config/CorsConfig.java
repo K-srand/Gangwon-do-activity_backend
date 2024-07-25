398439569aa5fe1a79a45d2443cb220dc52c6337
K-srand <ksuji9278@gmail.com>
2024-07-25 18:30:13 +0900

    빌드 에러 수정중

---
+++ src/main/java/com/multicampus/gangwonActivity/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.multicampus.gangwonActivity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://223.130.138.174:3030"));
+        configuration.setAllowedMethods(Arrays.asList("HEAD", "PUT", "POST", "GET", "OPTIONS", "DELETE", "PATCH"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        configuration.setAllowCredentials(true);
+        configuration.setExposedHeaders(Arrays.asList("Authorization", "Authorization-refresh"));
+        configuration.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
