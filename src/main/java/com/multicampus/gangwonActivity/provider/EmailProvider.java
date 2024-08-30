54d1575a4811d48dcad0e816863b59bc8b37a42d
K-srand <ksuji9278@gmail.com>
2024-08-30 11:35:18 +0900

    젠킨스 테스트

--- src/main/java/com/multicampus/gangwonActivity/provider/EmailProvider.java
+++ src/main/java/com/multicampus/gangwonActivity/provider/EmailProvider.java
@@ -49,7 +49,7 @@ public class EmailProvider {
             messageHelper.setTo(email);
             messageHelper.setSubject(findIdTitle);
             messageHelper.setText(htmlContent, true);
-
+            message.setFrom("skj2949@naver.com");
             javaMailSender.send(message);
 
         } catch (Exception exception) {
@@ -71,7 +71,7 @@ public class EmailProvider {
             messageHelper.setTo(email);
             messageHelper.setSubject(findPwdTitle);
             messageHelper.setText(htmlContent, true);
-
+            message.setFrom("skj2949@naver.com");
             javaMailSender.send(message);
 
         } catch (Exception exception) {
