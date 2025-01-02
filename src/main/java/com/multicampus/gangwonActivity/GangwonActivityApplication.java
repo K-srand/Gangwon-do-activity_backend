85991b70ceca02df045476e9b8bcaa79e01922da
K-srand <2125ks@naver.com>
2025-01-02 21:36:06 +0900

    fix : 환경변수 주입 에러 수정
    환경변수 주입 에러 수정하였습니다.

--- src/main/java/com/multicampus/gangwonActivity/GangwonActivityApplication.java
+++ src/main/java/com/multicampus/gangwonActivity/GangwonActivityApplication.java
@@ -3,10 +3,7 @@ package com.multicampus.gangwonActivity;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableScheduling
 @SpringBootApplication
--- src/main/java/com/multicampus/gangwonActivity/config/AppConfig.java
+++ src/main/java/com/multicampus/gangwonActivity/config/AppConfig.java
@@ -3,12 +3,7 @@ package com.multicampus.gangwonActivity.config;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.ArrayList;
-import java.util.List;
 
 //API 호출
 @Configuration
